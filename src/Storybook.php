2412f75e0bd86b838c4d3444aac821cd75cedeb2
Brian Hanson <brian@brianhanson.net>
2022-09-24 15:04:35 -0500

    Cleanup

--- src/Storybook.php
+++ src/Storybook.php
@@ -2,17 +2,13 @@
 
 namespace brianjhanson\storybook;
 
-use brianjhanson\storybook\models\Settings;
-use brianjhanson\storybook\services\Story;
-use brianjhanson\storybook\twigextensions\StorybookTwigExtension;
-use Craft;
 use craft\base\Plugin;
 use craft\events\RegisterUrlRulesEvent;
 use craft\web\UrlManager;
 use yii\base\Event;
 
 /**
- * @property Story $story
+ *
  */
 class Storybook extends Plugin
 {
@@ -24,7 +20,7 @@ class Storybook extends Plugin
     /**
      * @inerhitdoc
      */
-    public bool $hasCpSettings = true;
+    public bool $hasCpSettings = false;
 
     /**
      * @inheritdoc
@@ -35,42 +31,12 @@ class Storybook extends Plugin
         parent::init();
         self::$plugin = $this;
 
-        // Add in our Twig extension
-        $extension = new StorybookTwigExtension();
-        Craft::$app->view->registerTwigExtension($extension);
-
         Event::on(
             UrlManager::class,
             UrlManager::EVENT_REGISTER_SITE_URL_RULES,
             function(RegisterUrlRulesEvent $event) {
                 $event->rules['storybook/preview/<component:.+>'] = 'storybook/stories/preview';
             }
-        );
-
-        $this->setComponents([
-            'story' => Story::class
-        ]);
-    }
-
-
-    /**
-     * @inheritdoc
-     */
-    protected function createSettingsModel(): Settings
-    {
-        return new Settings();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function settingsHtml(): string
-    {
-        return Craft::$app->view->renderTemplate(
-            'storybook/settings',
-            [
-                'settings' => $this->getSettings()
-            ]
         );
     }
 }
